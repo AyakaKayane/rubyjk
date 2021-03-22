63df296521753f77c1344f2c5c73b6db07d835b8
EC2 Default User <ec2-user@ip-172-31-32-39.ap-northeast-1.compute.internal>
2021-03-22 15:48:04 +0000

    修正

--- jk.rb
+++ jk.rb
@@ -12,16 +12,27 @@ def janken
   if player_hand == program_hand
     puts "あいこで"
     return true
+    
   elsif(player_hand == 0 && program_hand == 1)||(player_hand == 1 && program_hand == 2)||(player_hand == 2 && program_hand == 0)
     puts "あっち向いてホイ！"
       puts "0(上) 1(下) 2(右) 3(左)"
       player_hand = gets.to_i
       program_hand = rand(4)
       
+      if player_hand == program_hand
       fingers = ["0(上)","1(下)","2(右)","3(左)"]
       puts "私→#{fingers[player_hand]},相手→#{fingers[program_hand]}"
-    puts "あなたの勝ちです"
-    return false
+      puts "あなたの勝ちです"
+      
+      elsif 
+        player_hand != program_hand
+        fingers = ["0(上)","1(下)","2(右)","3(左)"]
+      puts "私→#{fingers[player_hand]},相手→#{fingers[program_hand]}"
+        puts "あいこで"
+        return true
+    end 
+    
+
 
   else(player_hand == 0 && program_hand == 2)||(player_hand == 1 && program_hand == 0)||(player_hand == 2 && program_hand == 1)
     puts "あっち向いてホイ！"
@@ -29,10 +40,19 @@ def janken
       player_hand = gets.to_i
       program_hand = rand(4)
       
+      if player_hand == program_hand
+        
       fingers = ["0(上)","1(下)","2(右)","3(左)"]
       puts "私→#{fingers[player_hand]},相手→#{fingers[program_hand]}"
-    puts "あなたの負けです"
-    return false
+      puts "あなたの負けです"
+      
+      elsif
+        player_hand != program_hand
+        fingers = ["0(上)","1(下)","2(右)","3(左)"]
+      puts "私→#{fingers[player_hand]},相手→#{fingers[program_hand]}"
+        puts "あいこで"
+        return true
+    end 
   end
 end
 
